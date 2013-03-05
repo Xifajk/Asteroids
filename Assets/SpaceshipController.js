11bd549ae703819d94bc0bc4a72e5604e2f48a38
Matthew Dalli <matthew_dalli@hotmail.com>
2013-03-05 09:05:14 +0100

    Added Turbo Speed

--- Assets/SpaceshipController.js
+++ Assets/SpaceshipController.js
@@ -14,11 +14,18 @@ function Start () {
 
 function Update () {
 
+speed = 10;
+
+//speed up
+if(Input.GetKey(KeyCode.Space)){
+	speed = 50;
+}
+
 //spaceship moves up and down
 transform.Translate(Vector3.down*speed*Time.deltaTime*Input.GetAxis("Vertical"));
+
 //rotating spaceship
 transform.Rotate(Vector3.forward*-90*Time.deltaTime*Input.GetAxis("Horizontal"));
-
 
 if(transform.position.x > (BorderController.rightmost-1))
 {
